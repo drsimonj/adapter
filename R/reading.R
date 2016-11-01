8ee50d46dfbb0af42d1e3d17ffb09efc15377b06
drsimonj <=>
2016-11-02 09:37:55 +1100

    Add devtools to packrat

---
+++ R/reading.R
@@ -0,0 +1,14 @@
+#' Read participant's session file
+#'
+#'
+#' @param participant_dir Character string defining the participant's directory
+#'   of log files
+#' @param session_file Character string of the session file name
+#' @inheritParams readr::read_tsv
+#' @param ... Additional arguments to pass to \code{\link[readr]{read_tsv}}
+read_session <- function(participant_dir,
+                         session_file = "session.tsv",
+                         col_names = c("var", "info"),
+                         ...) {
+  readr::read_tsv(stringr::str_c(participant_dir, session_file), col_names = var_names)
+}
