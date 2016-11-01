10052246467d44150607e8be9c41bd9723033187
drsimonj <=>
2016-11-02 09:43:52 +1100

    Update read_session

--- R/reading.R
+++ R/reading.R
@@ -1,11 +1,12 @@
 #' Read participant's session file
 #'
-#'
 #' @param participant_dir Character string defining the participant's directory
 #'   of log files
 #' @param session_file Character string of the session file name
 #' @inheritParams readr::read_tsv
 #' @param ... Additional arguments to pass to \code{\link[readr]{read_tsv}}
+#' @return A \code{\link[tibble]{tibble}}
+#' @export
 read_session <- function(participant_dir,
                          session_file = "session.tsv",
                          col_names = c("var", "info"),
