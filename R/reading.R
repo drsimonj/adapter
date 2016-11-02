883441c875b8521ebcdc0a021c777fa007a801c2
drsimonj <=>
2016-11-02 11:29:31 +1100

    Add read_stream

--- R/reading.R
+++ R/reading.R
@@ -1,19 +1,24 @@
-#' Read a user's log file
+#' Read a user's single log file
 #'
 #' Read a user's session or events log file that are expected to be contained in
-#' the top-level of the user's log files directory. These files are assumed to
-#' be tab-separated values without variable headers. Uniquely, they are assumed
-#' to contain:
+#' the top-level of the user's log files directory, or a single stream file
+#' expected to be in the streams/ directory. These files are assumed to be
+#' tab-separated values without variable headers. Uniquely, they are assumed to
+#' contain:
 #'
 #' \describe {
 #'  \item{session}{Variable names and values}
 #'  \item{events}{A timestamp and an event name. Tab-separated details about the
 #'  event can follow in some cases.}
+#'  \item{stream}{A timestamp and a value.}
 #' }
 #'
 #' @param user_dir Character string defining the user's log-file directory
-#' @param file_name Character string of the file name
+#' @param file_name Character string of the file name. Must include extension such as .tsv
+#' @param stream_dir Character string defining the directory in which stream log
+#'   files exist.
 #' @param col_names Vector of column names to be used.
+#' @param is_numeric Boolean indicating whether a stream variable is numeric.
 #' @return \code{\link[tibble]{tibble}}
 #' @name read_log
 NULL
@@ -26,8 +31,6 @@ read_session <- function(user_dir,
 
   # Append "/" to directory if required
   user_dir <- end_with_slash(user_dir)
-  #if (!stringr::str_detect(user_dir, "/$"))
-  #  user_dir <- stringr::str_c(user_dir, "/")
 
   # Read file
   readr::read_tsv(stringr::str_c(user_dir, file_name), col_names = col_names)
@@ -41,8 +44,6 @@ read_events <- function(user_dir,
 
   # Append "/" to directory if required
   user_dir <- end_with_slash(user_dir)
-  #if (!stringr::str_detect(user_dir, "/$"))
-  #  user_dir <- stringr::str_c(user_dir, "/")
 
   events <- readr::read_lines(stringr::str_c(user_dir, file_name)) %>%
     stringr::str_split_fixed("\t", 3) %>%
@@ -50,4 +51,28 @@ read_events <- function(user_dir,
   names(events) <- c("time", "event", "detail")  # Give variables names
   events <- events %>% dplyr::mutate(detail = stringr::str_split(detail, "\t"))  # Split details into a list (where appropriate)
   events
+}
+
+#' @rdname read_log
+#' @export
+read_stream <- function(user_dir,
+                        file_name,
+                        stream_dir = "streams/",
+                        is_numeric = TRUE) {
+
+  # Get variable name
+  stream_var <- stringr::str_replace(file_name, "\\..*$", "")
+
+  # Append "/" to directorys if required
+  user_dir <- end_with_slash(user_dir)
+  stream_dir <- end_with_slash(stream_dir)
+
+  stream <- stringr::str_c(user_dir, stream_dir, file_name) %>%
+              readr::read_tsv(col_names = c("time", stream_var), col_types = "dc")  # Note need to import all values as characters
+
+  if (is_numeric) {
+    stream[[stream_var]] <- as.numeric(stream[[stream_var]])
+  }
+
+  stream
 }
