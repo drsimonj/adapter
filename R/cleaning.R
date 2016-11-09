cfdbf5bdf633695219341e48153b1d21b6d0538f
drsimonj <=>
2016-11-09 13:17:45 +1100

    Minor update to instruction

--- R/cleaning.R
+++ R/cleaning.R
@@ -1,8 +1,8 @@
 #' Clean lap information
 #'
-#' Add a `lap` number column to the events and streams tibbles and remove all
-#' data that precedes the start of the first lap, and comes after the end of the
-#' last lap. Only works for driver data.
+#' For driver data, add a `lap` number column to the events and streams tibbles
+#' and remove all data that precedes the start of the first lap, and comes after
+#' the end of the last lap.
 #'
 #' @param user A user list object created by reading functions like
 #'   \code{\link{read_logs}}
@@ -10,7 +10,7 @@
 #'   the simulation.
 #' @export
 #'
-clean_laps <- function(user, n_laps = 5) {
+clean_laps <- function(users, n_laps = 5) {
 
   # Handle events data
   user$events <- user$events %>%
@@ -19,11 +19,10 @@ clean_laps <- function(user, n_laps = 5) {
     dplyr::filter(dplyr::between(lap, 1, n_laps)) %>%   # Remove data pre/post test laps
     dplyr::select(lap, dplyr::everything())             # Reorder columns
 
-
   # Handle streams data
   start_time <- first(user$events$time)
   end_time   <- last(user$events$time)
-  lap_times <- user$events %>%
+  lap_times  <- user$events %>%
     dplyr::filter(event == "newlap") %>%
     dplyr::select(time, lap)
 
