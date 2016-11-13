ba90b6f00b0f6a9e12ab012f79c7c5cb60b786ae
drsimonj <=>
2016-11-14 10:18:07 +1100

    Enable clean_events to handle full user_list

--- R/user_list.R
+++ R/user_list.R
@@ -1,11 +1,35 @@
 #' @export
 clean_laps.user_list <- function(user, n_laps = 5) {
 
+  # Keep original class
+  oc <- class(user)
+
   # Handle drivers
   user <- purrr::map_if(user, ~is(., "driver"), clean_laps, n_laps = n_laps)
 
   # Handle drones
   user <- purrr::map_if(user, ~is(., "drone"), clean_laps, full_list = user)
+
+  # Reapply original class
+  class(user) <- oc
+
+  user
+}
+
+#' @export
+clean_events.user_list <- function(user) {
+
+  # Keep original class
+  oc <- class(user)
+
+  # Handle drivers
+  user <- purrr::map_if(user, ~is(., "driver"), clean_events)
+
+  # Handle drones
+  user <- purrr::map_if(user, ~is(., "drone"), clean_events, full_list = user)
+
+  # Reapply original class
+  class(user) <- oc
 
   user
 }
