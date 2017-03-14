c6ea5911bf8f3b52d38edea47a257ed05420e2f8
drsimonj <=>
2017-03-15 09:44:52 +1100

    Add: docs for waypoints data
    Added documentation describing the waypoints data, enabling ?waypoints

--- R/data.R
+++ R/data.R
@@ -1,7 +1,28 @@
+#' Waypoints defining the simulation route
+#'
+#' A dataset containing the waypoints that define the route taken by drivers
+#' during the simulation. The data includes the lap number and x and y
+#' coordinates of each waypoint. Each waypoint refers to a corner in the
+#' simulation at which the driver must make a turn. This can be at t-shaped
+#' intersections or an L-shaped section of the road that appears on the
+#' outer-most sections.
+#'
+#' @format A data frame (tibble) with 41 rows and 3 variables:
+#' \describe{
+#'   \item{lap}{Lap number that driver is on at this waypoint}
+#'   \item{x}{x-coordinate in the simulated world. Entire world spanned from 0
+#'   to 1200, with intersections and corners being found at 200 to 1000 in
+#'   increments of 200.}
+#'   \item{y}{y-coordinate in the simulated world. Entire world spanned from 0
+#'   to 1200, with intersections and corners being found at 200 to 1000 in
+#'   increments of 200.}
+#' }
+"waypoints"
+
 #' This commented out section contains the code used to create the
 #' sim_lap_routes tibble exported with the package as data
 
-# sim_lap_routes <- tibble::tribble(
+# waypoints <- tibble::tribble(
 #   ~lap,  ~x,  ~z,
 #   1, 600, 200,
 #   1, 600, 400,
@@ -45,4 +66,4 @@
 #   5, 800, 200,
 #   5, 600, 200
 # )
-# devtools::use_data(sim_lap_routes)
+# devtools::use_data(waypoints)
